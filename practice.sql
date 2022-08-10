df525f9b078d77bd3d420a82e97594d08bcf88eb
John Xander <johnblox5@gmail.com>
2022-08-10 10:57:14 +0100

    Completed

--- practice.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE reviews (
-    id BIGSERIAL NOT NULL PRIMARY KEY,
-    restaurant_id BIGINT NOT NULL REFERENCES restaurants(id),
-    name VARCHAR(50) NOT NULL,
-    review TEXT NOT NULL,
-    rating INT NOT NULL check(rating >= 1 and rating <= 5)
-);
-
-select * from restaurants left join (select restaurant_id, COUNT(*), trunc(AVG(rating), 1) as average_rating from reviews group by restaurant_id) reviews on restaurants.id = reviews.restaurant_id where id = $1;
